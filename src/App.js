0a0fde81ec9c8041a3ff00f3a7bdd99e206dd695
WaelS87 <wfakhrji@gmail.com>
2022-12-27 15:18:23 -0300

    create components

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Header from "./components/Header/Header";
+import Container from "./components/Container/Container";
+import Footer from "./components/Footer/Footer";
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Container />
+      <Footer />
+    </>
+  );
+};
+export default App;
