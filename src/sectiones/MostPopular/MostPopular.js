6d907ee2ab7bb5bee78648a8cef546210cc27e4f
WaelS87 <wfakhrji@gmail.com>
2022-12-28 16:56:12 -0300

    most popular component

--- src/sectiones/MostPopular/MostPopular.js
+++ src/sectiones/MostPopular/MostPopular.js
@@ -1,8 +1,29 @@
-import './MostPopular'
+import './MostPopular.css';
 
+
+import {Card} from '../../components/index.js'
 const MostPopular = () => {
   return (
-    <div>MostPopular</div>
+  <>
+    <div className='section-wrapper'>
+      <div className='section-header'>
+        <h4> Most Popular</h4>
+
+      </div>
+      <div className='most-popular-items'>
+        <Card image='popular_01' title='Fortnite'   category='Sandbox'    rate='3.8'   download='2.3M'/>
+        <Card image='popular_02' title='PubG'       category='Stream-X'   rate='4.8'   download='1.1M'/>
+        <Card image='popular_03' title='Dota2'      category='Legendary'  rate='2.7'   download='5.3M'/>
+        <Card image='popular_04' title='CS-Go'      category='Battle 5'   rate='3.9'   download='6.1M'/>
+        <Card image='popular_01' title='Fortnite'   category='Sandbox'    rate='3.8'   download='2.3M'/>
+        <Card image='popular_02' title='PubG'       category='Stream-X'   rate='4.8'   download='1.1M'/>
+        <Card image='popular_03' title='Dota2'      category='Legendary'  rate='2.7'   download='5.3M'/>
+        <Card image='popular_04' title='CS-Go'      category='Battle 5'   rate='3.9'   download='6.1M'/>
+       
+      </div>
+
+    </div>
+  </>
   )
 }
 
